9a8b55d66abc7bac18006e80f4b2d17107be2f15
Mionder <49869397+Mionder@users.noreply.github.com>
2020-08-15 13:46:34 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Header />
+    <Article />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Article from "./components/Article/Article.vue";
+import Header from "./components/Header/Header.vue";
+import Footer from "./components/Footer/Footer.vue";
+import "./reset.css";
+export default {
+  name: "App",
+  components: {
+    Header,
+    Article,
+    Footer
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
